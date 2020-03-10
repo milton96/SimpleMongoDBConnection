d6900020c63839d20f1ce04e6ec2ff25ddd9a65b
Milton Bravo <miltonbravo96@gmail.com>
2020-03-09 21:52:15 -0600

    Inicio

--- SimpleMongoDBConnection/Program.cs
+++ SimpleMongoDBConnection/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace SimpleMongoDBConnection
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola mundo!");
         }
     }
 }
